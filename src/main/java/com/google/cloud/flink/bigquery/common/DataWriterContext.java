f596a053f8d952e78d25ae9f2644db994bb9917e
sridhar durgam <sridhar.durgam@quantiphi.com>
2022-04-06 15:58:57 +0530

    Flink BQ connector write part

--- src/main/java/com/google/cloud/flink/bigquery/common/DataWriterContext.java
+++ src/main/java/com/google/cloud/flink/bigquery/common/DataWriterContext.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *       https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,17 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.cloud.flink.bigquery;
+package com.google.cloud.flink.bigquery.common;
 
 import java.io.IOException;
 
-public class FlinkBigQueryException extends RuntimeException {
+/**
+ * An internal version to Spark DataSource DataWriter interface
+ *
+ * @param <T>
+ */
+public interface DataWriterContext<T> {
+  void write(T row) throws IOException;
 
-  public FlinkBigQueryException(String message, Throwable error) {
-    super(message, error);
-  }
+  WriterCommitMessageContext finalizeStream() throws IOException;
 
-  public FlinkBigQueryException(String message) throws IOException {
-    super(message);
-  }
+  void commit() throws IOException;
+
+  void abort() throws IOException;
 }
